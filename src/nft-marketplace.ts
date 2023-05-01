49da8e05109aa602f36b0b7de42f186328c3672c
tmpITK <torok.mark.patrik@gmail.com>
2023-05-01 17:56:22 +0200

    Customize mapper

--- src/nft-marketplace.ts
+++ src/nft-marketplace.ts
@@ -1,126 +1,151 @@
 import {
-  NftListed as NftListedEvent,
-  NftListingCancelled as NftListingCancelledEvent,
-  NftListingUpdated as NftListingUpdatedEvent,
-  NftMinted as NftMintedEvent,
-  NftRequested as NftRequestedEvent,
-  NftSold as NftSoldEvent,
-  NftMarketplaceOwnershipTransferred as NftMarketplaceOwnershipTransferredEvent
+    NftListed as NftListedEvent,
+    NftListingCancelled as NftListingCancelledEvent,
+    NftListingUpdated as NftListingUpdatedEvent,
+    NftMinted as NftMintedEvent,
+    NftRequested as NftRequestedEvent,
+    NftSold as NftSoldEvent,
+    NftMarketplaceOwnershipTransferred as NftMarketplaceOwnershipTransferredEvent,
 } from "../generated/NftMarketplace/NftMarketplace"
 import {
-  NftListed,
-  NftListingCancelled,
-  NftListingUpdated,
-  NftMinted,
-  NftRequested,
-  NftSold,
-  NftMarketplaceOwnershipTransferred
+    NftListed,
+    NftListingCancelled,
+    NftListingUpdated,
+    NftMinted,
+    NftRequested,
+    NftSold,
+    NftMarketplaceOwnershipTransferred,
 } from "../generated/schema"
+import { BigInt, Address, Bytes } from "@graphprotocol/graph-ts"
+import { store } from "@graphprotocol/graph-ts"
 
 export function handleNftListed(event: NftListedEvent): void {
-  let entity = new NftListed(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.nftId = event.params.nftId
-  entity.owner = event.params.owner
-  entity.price = event.params.price
-  entity.ierc721TokenAddress = event.params.ierc721TokenAddress
+    let nftListed = NftListed.load(
+        getIdFromEventParamsAsBytes(event.params.nftId, event.params.ierc721TokenAddress)
+    )
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    if (!nftListed) {
+        nftListed = new NftListed(
+            getIdFromEventParamsAsBytes(event.params.nftId, event.params.ierc721TokenAddress)
+        )
+    }
+    nftListed.nftId = event.params.nftId
+    nftListed.owner = event.params.owner
+    nftListed.price = event.params.price
+    nftListed.ierc721TokenAddress = event.params.ierc721TokenAddress
 
-  entity.save()
+    nftListed.blockNumber = event.block.number
+    nftListed.blockTimestamp = event.block.timestamp
+    nftListed.transactionHash = event.transaction.hash
+
+    nftListed.save()
 }
 
-export function handleNftListingCancelled(
-  event: NftListingCancelledEvent
-): void {
-  let entity = new NftListingCancelled(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.nftId = event.params.nftId
-  entity.owner = event.params.owner
-  entity.ierc721TokenAddress = event.params.ierc721TokenAddress
+export function handleNftListingCancelled(event: NftListingCancelledEvent): void {
+    let nftListed = NftListed.load(
+        getIdFromEventParamsAsBytes(event.params.nftId, event.params.ierc721TokenAddress)
+    )
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    let nftListingCancelled = NftListingCancelled.load(
+        getIdFromEventParamsAsBytes(event.params.nftId, event.params.ierc721TokenAddress)
+    )
 
-  entity.save()
+    if (!nftListingCancelled) {
+        nftListingCancelled = new NftListingCancelled(
+            getIdFromEventParamsAsBytes(event.params.nftId, event.params.ierc721TokenAddress)
+        )
+    }
+
+    nftListingCancelled.nftId = event.params.nftId
+    nftListingCancelled.owner = event.params.owner
+    nftListingCancelled.ierc721TokenAddress = event.params.ierc721TokenAddress
+
+    nftListingCancelled.blockNumber = event.block.number
+    nftListingCancelled.blockTimestamp = event.block.timestamp
+    nftListingCancelled.transactionHash = event.transaction.hash
+
+    if (!nftListed) {
+        return
+    }
+
+    // Is this okay? I want to remove any cancelled stuff so I cannot query it anymore
+    store.remove(
+        "NftListed",
+        getIdFromEventParams(event.params.nftId, event.params.ierc721TokenAddress)
+    )
 }
 
 export function handleNftListingUpdated(event: NftListingUpdatedEvent): void {
-  let entity = new NftListingUpdated(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.nftId = event.params.nftId
-  entity.owner = event.params.owner
-  entity.price = event.params.price
-  entity.ierc721TokenAddress = event.params.ierc721TokenAddress
+    let entity = new NftListingUpdated(event.transaction.hash.concatI32(event.logIndex.toI32()))
+    entity.nftId = event.params.nftId
+    entity.owner = event.params.owner
+    entity.price = event.params.price
+    entity.ierc721TokenAddress = event.params.ierc721TokenAddress
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    entity.blockNumber = event.block.number
+    entity.blockTimestamp = event.block.timestamp
+    entity.transactionHash = event.transaction.hash
 
-  entity.save()
+    entity.save()
 }
 
 export function handleNftMinted(event: NftMintedEvent): void {
-  let entity = new NftMinted(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.tokenId = event.params.tokenId
+    let entity = new NftMinted(event.transaction.hash.concatI32(event.logIndex.toI32()))
+    entity.owner = event.params.owner
+    entity.tokenId = event.params.tokenId
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    entity.blockNumber = event.block.number
+    entity.blockTimestamp = event.block.timestamp
+    entity.transactionHash = event.transaction.hash
 
-  entity.save()
+    entity.save()
 }
 
 export function handleNftRequested(event: NftRequestedEvent): void {
-  let entity = new NftRequested(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.requester = event.params.requester
+    let entity = new NftRequested(event.transaction.hash.concatI32(event.logIndex.toI32()))
+    entity.requester = event.params.requester
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    entity.blockNumber = event.block.number
+    entity.blockTimestamp = event.block.timestamp
+    entity.transactionHash = event.transaction.hash
 
-  entity.save()
+    entity.save()
 }
 
 export function handleNftSold(event: NftSoldEvent): void {
-  let entity = new NftSold(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.owner = event.params.owner
-  entity.nftId = event.params.nftId
-  entity.ierc721TokenAddress = event.params.ierc721TokenAddress
-  entity.price = event.params.price
+    let entity = new NftSold(event.transaction.hash.concatI32(event.logIndex.toI32()))
+    entity.owner = event.params.owner
+    entity.nftId = event.params.nftId
+    entity.ierc721TokenAddress = event.params.ierc721TokenAddress
+    entity.price = event.params.price
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    entity.blockNumber = event.block.number
+    entity.blockTimestamp = event.block.timestamp
+    entity.transactionHash = event.transaction.hash
 
-  entity.save()
+    entity.save()
 }
 
 export function handleNftMarketplaceOwnershipTransferred(
-  event: NftMarketplaceOwnershipTransferredEvent
+    event: NftMarketplaceOwnershipTransferredEvent
 ): void {
-  let entity = new NftMarketplaceOwnershipTransferred(
-    event.transaction.hash.concatI32(event.logIndex.toI32())
-  )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+    let entity = new NftMarketplaceOwnershipTransferred(
+        event.transaction.hash.concatI32(event.logIndex.toI32())
+    )
+    entity.previousOwner = event.params.previousOwner
+    entity.newOwner = event.params.newOwner
 
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+    entity.blockNumber = event.block.number
+    entity.blockTimestamp = event.block.timestamp
+    entity.transactionHash = event.transaction.hash
 
-  entity.save()
+    entity.save()
+}
+
+function getIdFromEventParams(tokenId: BigInt, nftAddress: Address): string {
+    return tokenId.toHexString() + nftAddress.toHexString()
+}
+
+function getIdFromEventParamsAsBytes(tokenId: BigInt, nftAddress: Address): Bytes {
+    return new Bytes(parseInt(tokenId.toHexString() + nftAddress.toHexString(), 16))
 }
