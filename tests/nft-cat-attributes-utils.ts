bfa4268810435484cc018e7f175a133fea5fd238
tmpITK <torok.mark.patrik@gmail.com>
2023-05-03 17:05:35 +0200

    Add matchstick - not working

--- tests/nft-cat-attributes-utils.ts
+++ tests/nft-cat-attributes-utils.ts
@@ -1,104 +1,80 @@
 import { newMockEvent } from "matchstick-as"
 import { ethereum, BigInt, Address } from "@graphprotocol/graph-ts"
 import {
-  NftCatAttributesCreated,
-  NftCatAttributesRequested,
-  OwnershipTransferred
+    NftCatAttributesCreated,
+    NftCatAttributesRequested,
+    OwnershipTransferred,
 } from "../generated/NftCatAttributes/NftCatAttributes"
 
 export function createNftCatAttributesCreatedEvent(
-  requestId: BigInt,
-  requester: Address,
-  breed: i32,
-  eyecolor: i32,
-  playfulness: BigInt,
-  cuteness: BigInt
+    requestId: BigInt,
+    requester: Address,
+    breed: i32,
+    eyecolor: i32,
+    playfulness: BigInt,
+    cuteness: BigInt
 ): NftCatAttributesCreated {
-  let nftCatAttributesCreatedEvent = changetype<NftCatAttributesCreated>(
-    newMockEvent()
-  )
+    let nftCatAttributesCreatedEvent = changetype<NftCatAttributesCreated>(newMockEvent())
 
-  nftCatAttributesCreatedEvent.parameters = new Array()
+    nftCatAttributesCreatedEvent.parameters = new Array()
 
-  nftCatAttributesCreatedEvent.parameters.push(
-    new ethereum.EventParam(
-      "requestId",
-      ethereum.Value.fromUnsignedBigInt(requestId)
+    nftCatAttributesCreatedEvent.parameters.push(
+        new ethereum.EventParam("requestId", ethereum.Value.fromUnsignedBigInt(requestId))
     )
-  )
-  nftCatAttributesCreatedEvent.parameters.push(
-    new ethereum.EventParam("requester", ethereum.Value.fromAddress(requester))
-  )
-  nftCatAttributesCreatedEvent.parameters.push(
-    new ethereum.EventParam(
-      "breed",
-      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(breed))
+    nftCatAttributesCreatedEvent.parameters.push(
+        new ethereum.EventParam("requester", ethereum.Value.fromAddress(requester))
     )
-  )
-  nftCatAttributesCreatedEvent.parameters.push(
-    new ethereum.EventParam(
-      "eyecolor",
-      ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(eyecolor))
+    nftCatAttributesCreatedEvent.parameters.push(
+        new ethereum.EventParam("breed", ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(breed)))
     )
-  )
-  nftCatAttributesCreatedEvent.parameters.push(
-    new ethereum.EventParam(
-      "playfulness",
-      ethereum.Value.fromUnsignedBigInt(playfulness)
+    nftCatAttributesCreatedEvent.parameters.push(
+        new ethereum.EventParam(
+            "eyecolor",
+            ethereum.Value.fromUnsignedBigInt(BigInt.fromI32(eyecolor))
+        )
     )
-  )
-  nftCatAttributesCreatedEvent.parameters.push(
-    new ethereum.EventParam(
-      "cuteness",
-      ethereum.Value.fromUnsignedBigInt(cuteness)
+    nftCatAttributesCreatedEvent.parameters.push(
+        new ethereum.EventParam("playfulness", ethereum.Value.fromUnsignedBigInt(playfulness))
     )
-  )
+    nftCatAttributesCreatedEvent.parameters.push(
+        new ethereum.EventParam("cuteness", ethereum.Value.fromUnsignedBigInt(cuteness))
+    )
 
-  return nftCatAttributesCreatedEvent
+    return nftCatAttributesCreatedEvent
 }
 
 export function createNftCatAttributesRequestedEvent(
-  requestId: BigInt,
-  requester: Address
+    requestId: BigInt,
+    requester: Address
 ): NftCatAttributesRequested {
-  let nftCatAttributesRequestedEvent = changetype<NftCatAttributesRequested>(
-    newMockEvent()
-  )
+    let nftCatAttributesRequestedEvent = changetype<NftCatAttributesRequested>(newMockEvent())
 
-  nftCatAttributesRequestedEvent.parameters = new Array()
+    nftCatAttributesRequestedEvent.parameters = new Array()
 
-  nftCatAttributesRequestedEvent.parameters.push(
-    new ethereum.EventParam(
-      "requestId",
-      ethereum.Value.fromUnsignedBigInt(requestId)
+    nftCatAttributesRequestedEvent.parameters.push(
+        new ethereum.EventParam("requestId", ethereum.Value.fromUnsignedBigInt(requestId))
     )
-  )
-  nftCatAttributesRequestedEvent.parameters.push(
-    new ethereum.EventParam("requester", ethereum.Value.fromAddress(requester))
-  )
+    nftCatAttributesRequestedEvent.parameters.push(
+        new ethereum.EventParam("requester", ethereum.Value.fromAddress(requester))
+    )
 
-  return nftCatAttributesRequestedEvent
+    return nftCatAttributesRequestedEvent
 }
 
 export function createOwnershipTransferredEvent(
-  previousOwner: Address,
-  newOwner: Address
+    previousOwner: Address,
+    newOwner: Address
 ): OwnershipTransferred {
-  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
-    newMockEvent()
-  )
+    let ownershipTransferredEvent = changetype<OwnershipTransferred>(newMockEvent())
 
-  ownershipTransferredEvent.parameters = new Array()
+    ownershipTransferredEvent.parameters = new Array()
 
-  ownershipTransferredEvent.parameters.push(
-    new ethereum.EventParam(
-      "previousOwner",
-      ethereum.Value.fromAddress(previousOwner)
+    ownershipTransferredEvent.parameters.push(
+        new ethereum.EventParam("previousOwner", ethereum.Value.fromAddress(previousOwner))
     )
-  )
-  ownershipTransferredEvent.parameters.push(
-    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
-  )
+    ownershipTransferredEvent.parameters.push(
+        new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+    )
 
-  return ownershipTransferredEvent
+    return ownershipTransferredEvent
 }
